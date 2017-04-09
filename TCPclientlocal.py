27197d40edad43eece72409d725fad150368fda4
Matthew Biesen <upandcomming88@gmail.com>
2017-04-09 15:21:45 -0500

    Add files via upload

---
+++ TCPclientlocal.py
@@ -0,0 +1,19 @@
+import socket
+
+target_host = "0.0.0.0"
+target_port = 9999
+
+#create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#connect the client
+client.connect((target_host, target_port))
+
+#send some data
+client.send("hello")
+
+#receive some data
+response = client.recv(4096)
+
+print(response)
+    
